5dc9fbc868b24912880b6b6b115b3d833ec9701e
Mikhail Lipilov <m.lipilov@Mikhails-MacBook-Pro.local>
2022-09-15 01:24:24 +0300

    fdlkgjnekjgejkdfngejkf

---
+++ backup/deploy.sh
@@ -0,0 +1,32 @@
+#bin/bash
+
+#Building images for setting up local k8s cluster
+cd ./client; docker build -t home1docker1/multi-client:latest -f Dockerfile .
+cd ..; cd ./server; docker build -t home1docker1/multi-server:latest -f Dockerfile . 
+cd ..; cd ./worker; docker build -t home1docker1/multi-worker:latest -f Dockerfile .
+
+#Building images for setting up prod k8s cluster
+cd..; cd ./client; docker build -t home1docker1/multi-client:$COMMIT_SHA -f Dockerfile .
+cd ..; cd ./server; docker build -t home1docker1/multi-server:$COMMIT_SHA -f Dockerfile . 
+cd ..; cd ./worker; docker build -t home1docker1/multi-worker:$COMMIT_SHA -f Dockerfile .
+
+#Pushing images(local purposes) 
+docker push home1docker1/multi-client:latest
+docker push home1docker1/multi-server:latest
+docker push home1docker1/multi-worker:latest
+
+#Pushing images(prod purposes)
+docker push home1docker1/multi-client:$COMMIT_SHA
+docker push home1docker1/multi-server:$COMMIT_SHA
+docker push home1docker1/multi-worker:$COMMIT_SHA
+
+#Deploying k8s objects
+cd ..; cd ./k8s; kubectl apply -f deployment/
+kubectl apply -f ingress/
+kubectl apply -f pv/
+kubectl apply -f service/
+
+#Imperatively updating k8s deployments
+kubectl set image deployments/client-deployment multiapp-client=home1docker1/multi-client:$COMMIT_SHA
+kubectl set image deployments/server-deployment multiapp-server=home1docker1/multi-server:$COMMIT_SHA
+kubectl set image deployments/worker-deployment multiapp-worker=home1docker1/multi-worker:$COMMIT_SHA
--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,16 @@
-#bin/bash
+docker build -t home1docker11/multi-client-k8s:latest -t home1docker11/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t home1docker11/multi-server-k8s-pgfix:latest -t home1docker11/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t home1docker11/multi-worker-k8s:latest -t home1docker11/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-#Building images for setting up local k8s cluster
-cd ./client; docker build -t home1docker1/multi-client:latest -f Dockerfile .
-cd ..; cd ./server; docker build -t home1docker1/multi-server:latest -f Dockerfile . 
-cd ..; cd ./worker; docker build -t home1docker1/multi-worker:latest -f Dockerfile .
+docker push home1docker11/multi-client-k8s:latest
+docker push home1docker11/multi-server-k8s-pgfix:latest
+docker push home1docker11/multi-worker-k8s:latest
 
-#Building images for setting up prod k8s cluster
-cd..; cd ./client; docker build -t home1docker1/multi-client:$COMMIT_SHA -f Dockerfile .
-cd ..; cd ./server; docker build -t home1docker1/multi-server:$COMMIT_SHA -f Dockerfile . 
-cd ..; cd ./worker; docker build -t home1docker1/multi-worker:$COMMIT_SHA -f Dockerfile .
+docker push home1docker11/multi-client-k8s:$SHA
+docker push home1docker11/multi-server-k8s-pgfix:$SHA
+docker push home1docker11/multi-worker-k8s:$SHA
 
-#Pushing images(local purposes) 
-docker push home1docker1/multi-client:latest
-docker push home1docker1/multi-server:latest
-docker push home1docker1/multi-worker:latest
-
-#Pushing images(prod purposes)
-docker push home1docker1/multi-client:$COMMIT_SHA
-docker push home1docker1/multi-server:$COMMIT_SHA
-docker push home1docker1/multi-worker:$COMMIT_SHA
-
-#Deploying k8s objects
-cd ..; cd ./k8s; kubectl apply -f deployment/
-kubectl apply -f ingress/
-kubectl apply -f pv/
-kubectl apply -f service/
-
-#Imperatively updating k8s deployments
-kubectl set image deployments/client-deployment multiapp-client=home1docker1/multi-client:$COMMIT_SHA
-kubectl set image deployments/server-deployment multiapp-server=home1docker1/multi-server:$COMMIT_SHA
-kubectl set image deployments/worker-deployment multiapp-worker=home1docker1/multi-worker:$COMMIT_SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=home1docker11/multi-server-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=home1docker11/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=home1docker11/multi-worker-k8s:$SHA

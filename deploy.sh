c0ff4a6d87868b23e77377f70a624b16c093c728
Mikhail Lipilov <m.lipilov@Mikhails-MacBook-Pro.local>
2022-09-14 20:57:27 +0300

    fsfgkjnsf

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 
 #Building images for setting up local k8s cluster
 echo $PWD
-docker build -t home1docker1/multi-client:latest -f Dockerfile .
-cd ./server; docker build -t home1docker1/multi-server:latest -f Dockerfile . 
+cd ./client; docker build -t home1docker1/multi-client:latest -f Dockerfile .
+cd ..; cd ./server; docker build -t home1docker1/multi-server:latest -f Dockerfile . 
 cd ..; cd ./worker; docker build -t home1docker1/multi-worker:latest -f Dockerfile .
 
 #Building images for setting up prod k8s cluster
-docker build -t home1docker1/multi-client:$COMMIT_SHA -f Dockerfile .
+cd..; cd ./client; docker build -t home1docker1/multi-client:$COMMIT_SHA -f Dockerfile .
 cd ..; cd ./server; docker build -t home1docker1/multi-server:$COMMIT_SHA -f Dockerfile . 
 cd ..; cd ./worker; docker build -t home1docker1/multi-worker:$COMMIT_SHA -f Dockerfile .
 
@@ -23,9 +23,9 @@ docker push home1docker1/multi-worker:$COMMIT_SHA
 
 #Deploying k8s objects
 cd ..; cd ./k8s; kubectl apply -f deployment/
-cd ./k8s; kubectl apply -f ingress/
-cd ./k8s; kubectl apply -f pv/
-cd ./k8s; kubectl apply -f service/
+kubectl apply -f ingress/
+kubectl apply -f pv/
+kubectl apply -f service/
 
 #Imperatively updating k8s deployments
 kubectl set image deployments/client-deployment multiapp-client=home1docker1/multi-client:$COMMIT_SHA
